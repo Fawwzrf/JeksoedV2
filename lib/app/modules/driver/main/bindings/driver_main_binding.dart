0018ceec1541e90e378fc1d7a52d88bb65f48337
diaz287 <khafrizadiaz28@gmail.com>
2025-12-15 20:38:36 +0700

    `Added lazyPut for DriverHomeController`

--- lib/app/modules/driver/main/bindings/driver_main_binding.dart
+++ lib/app/modules/driver/main/bindings/driver_main_binding.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
 import '../controllers/driver_main_controller.dart';
 import '../../../shared/activity/controllers/activity_controller.dart';
+import '../../home/controllers/driver_home_controller.dart';
 import '../../../shared/profile/controllers/profile_controller.dart';
 
 class DriverMainBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<DriverMainController>(() => DriverMainController());
+    Get.lazyPut<DriverHomeController>(() => DriverHomeController());
     Get.lazyPut<ActivityController>(() => ActivityController());
     Get.lazyPut<ProfileController>(() => ProfileController());
   }
